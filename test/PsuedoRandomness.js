1ced8d6739a74f0715dc631ab9311a62b8694302
Sant Deleon <sant@santdeleon.co>
2020-01-24 11:31:14 -0600

    Create test cases for random() function

---
+++ test/PsuedoRandomness.js
@@ -0,0 +1,27 @@
+const PseudoRandomness = artifacts.require("PseudoRandomness");
+var truffleAssert = require("truffle-assertions");
+
+
+contract("PseudoRandomness", async (accounts) => {
+  const [owner, alice, bob] = accounts;
+
+  beforeEach(async () => {
+    instance = await PseudoRandomness.new();
+  });
+
+  context("when testing random number generation", async () => {
+    it("should return 0 if number is generated at an odd time", async () => {
+      let generatedAtOddTime;
+      let rand = parseInt(await instance.random({from: alice}));
+      (rand === 0) ? generatedAtOddTime = true : generatedAtOddTime = false;
+      truffleAssert.passes(generatedAtOddTime, truffleAssert.ErrorType.REVERT);
+    });
+
+    it("should return 1 if number is generated at an even time", async () => {
+      let generatedAtEvenTime;
+      let rand = parseInt(await instance.random({from: alice}));
+      (rand === 1) ? generatedAtEvenTime = true : generatedAtEvenTime = false;
+      truffleAssert.passes(generatedAtEvenTime, truffleAssert.ErrorType.REVERT);
+    });
+  });
+});
