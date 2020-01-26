4b606f9c3d96dc4a1e1b2ad7202653c80b302501
Sant Deleon <sant@santdeleon.co>
2020-01-25 21:43:10 -0600

    Import new loader and style Loader component

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Ethereum from '../../assets/gif/ethereum-loader.gif';
+import Ethereum from '../../assets/gif/ethereum-loader-2.gif';
 
 import './Loader.css';
 
@@ -9,8 +9,8 @@ function Loader() {
   return (
     <section className="Loader">
       <div className="row flex">
-        <div className="col flex justify-content-center mx-auto">
-          <img src={Ethereum} alt="Ethereum Glitch"/>
+        <div className="col flex flex-column align-items-center justify-content-center mx-auto">
+          <img src={Ethereum} className="mx-auto" alt="Ethereum Glitch"/>
         </div>
       </div>
     </section>
