a7b71af263ceaccbd597909be4cd58ea9e3c3f0d
Sant Deleon <sant@santdeleon.co>
2020-01-25 21:34:37 -0600

    Add some default css for flexbox based design and Initialize App.js

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import PseudoRandomnessContract from "../../contracts/PseudoRandomness.json";
+import CoinFlip from "../../contracts/CoinFlip.json";
 import getWeb3 from "../../getWeb3";
+import Loader from "../../components/Loader/Loader";
 
 import "./App.css";
 
@@ -17,15 +18,15 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = PseudoRandomnessContract.networks[networkId];
+      const deployedNetwork = CoinFlip.networks[networkId];
       const instance = new web3.eth.Contract(
-        PseudoRandomnessContract.abi,
+        CoinFlip.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,38 +36,24 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return (
+        <>
+          <Loader />
+        </>
+      );
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+      <>
+        <Loader />
+      </>
     );
+    {/*return (
+      <div className="App">
+        <div className="rainbow-top"></div>
+      </div>
+    );*/}
   }
 }
 
