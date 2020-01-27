9d3c35b12cc3169ab5e819a171a291905abd911c
Sant Deleon <sant@santdeleon.co>
2020-01-27 10:58:54 -0600

    Write successful test for the getContract method && annotate testing

--- test/CoinFlip.js
+++ test/CoinFlip.js
@@ -3,11 +3,15 @@ var truffleAssert = require("truffle-assertions");
 
 
 contract("CoinFlip", async (accounts) => {
+  // this takes the first 3 Ganache accounts and sets them equal to the accounts at that index
+  // accounts[0] = owner, accounts[1] = alice, accounts[2] = bob
   const [owner, alice, bob] = accounts;
   let instance;
+  // gasPrice and gasLimit are default values on the Ganache client
   const gasPrice = 20000000000;
   const gasLimit = 6721975;
 
+  // before each test instantiate a new CoinFlip instance
   beforeEach(async () => {
     instance = await CoinFlip.new();
   });
@@ -15,7 +19,18 @@ contract("CoinFlip", async (accounts) => {
   context("when deploying the contract", async () => {
     it("should set the deployer of the contract as the owner of the contract", async () => {
       let ownerLookup = await instance.getOwner({from: alice});
-      truffleAssert.passes(ownerLookup[0] === accounts[0], truffleAssert.ErrorType.REVERT);
+      truffleAssert.passes(ownerLookup[0] === owner, truffleAssert.ErrorType.REVERT);
+    });
+  });
+
+  context("when retrieving contract info", async () => {
+    it("should return the contract address and contract balance", async () => {
+      let addressMatches, balanceMatches;
+
+      let contractLookup = await instance.getContract({from: alice});
+      (contractLookup[0] === instance.address) ? addressMatches = true : addressMatches = false;
+      (parseInt(contractLookup[1]) === parseInt(await web3.eth.getBalance(instance.address))) ? balanceMatches = true : balanceMatches = false;
+      truffleAssert.passes(addressMatches, balanceMatches, truffleAssert.ErrorType.REVERT);
     });
   });
 
