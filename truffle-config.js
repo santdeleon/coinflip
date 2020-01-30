2179e5f929b60d1580a2947f4f277619b62fa7e4
Sant Deleon <sant@santdeleon.co>
2020-01-30 00:02:40 -0600

    Install hdwallet provider && add .gitignore for .secret files && deploy contract to ropsten testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const projectId = "587c0d97f7ae4e67aeed916ef391ce39";
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,7 +10,16 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 7545
+      provider: () => new HDWalletProvider(mnemonic, "http://127.0.0.1:7545"),
+      network_id: "*"
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   }
 };
