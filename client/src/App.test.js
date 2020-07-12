d9a3d595ff965445e25573dbe53445937967598e
Sant Deleon <sant@santdeleon.co>
2020-07-11 23:55:12 -0500

    test: write tests for <App />

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { toBeInTheDocument } from "@testing-library/jest-dom";
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 
-import getWeb3 from "./components/getWeb3";
-import Loader from "./components/Loader";
 import App from "./App";
 
-it("show loading screen while retrieving data from the blockchain and then render <App /> ", async () => {
+it("should render a loading screen while retrieving data from the blockchain and then render <App />", async () => {
   // show loader
-  render(<Loader />);
+  render(<App />);
   expect(screen.queryByTestId("Loader")).toBeInTheDocument();
 
-  // ping the blockchain
-  await getWeb3();
-
-  // render <App />
-  render(<App />);
-  expect(screen.queryByTestId("App")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("App")).toBeInTheDocument();
+  });
 });
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
