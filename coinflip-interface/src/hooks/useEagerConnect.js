264a4d746273f0d7f8a6457883d2b2630547fa32
Sant Deleon <sant@santdeleon.io>
2020-12-27 18:32:26 -0600

    perf: make useEagerConnect hook efficient

--- coinflip-interface/src/hooks/useEagerConnect.js
+++ coinflip-interface/src/hooks/useEagerConnect.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useWeb3React as useWeb3ReactCore } from '@web3-react/core';
 
 import { injected } from '../connectors';
 
 export const useEagerConnect = () => {
+  let mounted = useRef(false);
   const [tried, setTried] = useState(false);
   const { activate, active } = useWeb3ReactCore(); // specifically using useWeb3ReactCore because of what this hook does
 
@@ -16,9 +17,8 @@ export const useEagerConnect = () => {
 
   // if the connection worked, wait until we get confirmation to flip the flag
   useEffect(() => {
-    if (active) {
-      setTried(true);
-    }
+    if (active && mounted.current) setTried(true);
+    return () => (mounted.current = false);
   }, [active]);
 
   return tried;
