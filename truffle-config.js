4390d7fa79dacd92509f896f3b001578ddda146b
Sant Deleon <sant@santdeleon.io>
2021-01-01 14:39:27 -0600

    build: make truffle-config more dynamic
    truffle config can now use mnemonics

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,17 @@
 const path = require("path");
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(
+    __dirname,
+    "coinflip-interface/src/contracts"
+  ),
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, "http://127.0.0.1:8545"),
       network_id: "*",
-      websockets: true,
     },
   },
 };
