64ca5db05e2cd25e9140414b288bcdb6496a27c2
Sant Deleon <sant@santdeleon.io>
2021-01-02 12:22:06 -0600

    style: make the Emoji component more dynamic

--- coinflip-interface/src/components/Emoji.js
+++ coinflip-interface/src/components/Emoji.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { string } from 'prop-types';
+import styled from 'styled-components';
+
+const StyledEmoji = styled.span`
+  margin: ${({ margin }) => (margin ? margin : '0 0.3rem')};
+  padding: ${({ padding }) => padding};
+`;
 
 const propTypes = {
   ariaLabel: string.isRequired,
   unicode: string.isRequired,
+  margin: string,
+  padding: string,
   className: string,
 };
 
-const Emoji = ({ ariaLabel, unicode, className }) => (
-  <span role="img" alt={ariaLabel} aria-label={ariaLabel} className={className}>
+const Emoji = ({
+  ariaLabel,
+  unicode,
+  margin,
+  padding,
+  className,
+  ...props
+}) => (
+  <StyledEmoji
+    role="img"
+    alt={ariaLabel}
+    aria-label={ariaLabel}
+    margin={margin}
+    padding={padding}
+    className={className}
+    {...props}
+  >
     {unicode}
-  </span>
+  </StyledEmoji>
 );
 
 Emoji.propTypes = propTypes;
