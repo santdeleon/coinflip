d4bfe03803601585371b438272d85acdc9dd4af6
Sant Deleon <sant@santdeleon.io>
2021-01-02 12:43:16 -0600

    style: fix ToggleSwitch styles that broke due to removal of bootstrap css

--- coinflip-interface/src/components/ToggleSwitch.js
+++ coinflip-interface/src/components/ToggleSwitch.js
@@ -4,6 +4,9 @@ import { func, string, oneOfType, array, object } from 'prop-types';
 import { useTheme } from '../hooks';
 
 const StyledToggleThumb = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: absolute;
   top: -4px;
   left: -3px;
@@ -21,10 +24,14 @@ const StyledToggleThumb = styled.div`
 const StyledToggleTrack = styled.span`
   position: relative;
   display: inline-block;
-  width: 42px;
-  height: 24px;
+  width: 38px;
+  height: 20px;
   border-radius: 34px;
   outline: 0;
+  /**
+    * @dev: Eventually we could make the coloration for the switches dynamic and
+    * not specific to just the theme toggle. Currently this isn't neccesary.
+    */
   border: 3px solid
     ${({ theme }) => (theme === 'light' ? '#d1d5da' : '#3c1e70')};
   background-color: ${({ theme }) =>
@@ -35,18 +42,17 @@ const propTypes = {
   id: string.isRequired,
   className: string,
   title: string.isRequired,
-  ariaLabel: string.isRequired,
   onClick: func,
   children: oneOfType([array, object, string]),
 };
 
 const ToggleSwitch = ({
   id,
-  className,
   title,
-  ariaLabel,
+  className,
   onClick,
   children,
+  ...props
 }) => {
   const { theme } = useTheme();
 
@@ -54,12 +60,13 @@ const ToggleSwitch = ({
     <StyledToggleTrack
       id={id}
       role="checkbox"
-      className={className}
       title={title}
-      aria-label={ariaLabel}
+      aria-label={title}
       aria-checked="false"
       tabIndex="0"
+      className={className}
       theme={theme}
+      {...props}
     >
       <StyledToggleThumb theme={theme} onClick={onClick}>
         {children}
